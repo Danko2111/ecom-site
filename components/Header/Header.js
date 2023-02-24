c0edb67005e00d5c158f9bfeb9f66c73a0d26967
Daniel <daniel.kolesnitski@gmail.com>
2023-02-23 16:27:59 -0800

    migrated project to use SASS

---
+++ components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "../../styles/components/Header/Header.module.scss";
+
+function header() {
+  return <div className={style.header}>header</div>;
+}
+
+export default header;
